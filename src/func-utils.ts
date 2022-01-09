4a6002f42ffcb57e5da1cdf499d73a052a73c85a
Meinte Boersma <meinte.boersma@gmail.com>
2022-01-09 21:40:51 +0100

    (replace function syntax with generic arrow syntax)

--- src/func-utils.ts
+++ src/func-utils.ts
@@ -1,11 +1,9 @@
-export function last<T>(arr: T[]): T {
-    return arr[arr.length - 1]
-}
-
-export function reduceAccumulatively<AT, VT>(lambda: (acc: AT, value: VT) => AT, init: AT): ((vs: VT[]) => AT[]) {
-    return (vs: VT[]) =>
-        vs.reduce((acc: AT[], v: VT) => [...acc, lambda(last(acc), v)], [init])
-}
+export const last = <T>(arr: T[]): T =>
+    arr[arr.length - 1]
 
 // TODO  type s.t. last(..) receives a non-empty array
+
+export const reduceAccumulatively = <AT, VT>(lambda: (acc: AT, value: VT) => AT, init: AT): ((vs: VT[]) => AT[]) =>
+    (vs: VT[]) =>
+        vs.reduce((acc: AT[], v: VT) => [...acc, lambda(last(acc), v)], [init])
 
